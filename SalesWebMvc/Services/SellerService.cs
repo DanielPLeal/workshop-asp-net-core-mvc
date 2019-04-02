abbd77aa6784e025326452df2939f2befa55fdff
Daniel Leal <danielpleal@gmail.com>
2019-04-01 23:28:53 -0300

    Exception handling for delete (referential integrity)

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -17,9 +17,9 @@ namespace SalesWebMvc.Services
             _context = context;
         }
 
-        public  async Task<List<Seller>> FindAllAsync()
+        public async Task<List<Seller>> FindAllAsync()
         {
-            return  await _context.Seller.ToListAsync();
+            return await _context.Seller.ToListAsync();
         }
 
         public async Task InsertAsync(Seller obj)
@@ -31,18 +31,25 @@ namespace SalesWebMvc.Services
         public async Task<Seller> FindByIdAsync(int id)
         {
             return await _context.Seller.Include(obj => obj.Department).FirstOrDefaultAsync(obj => obj.Id == id);
-        }         
+        }
 
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch(DbUpdateException e)
+            {
+                throw new IntegrityException(e.Message);
+            }
         }
 
         public async Task UpdateAsync(Seller obj)
         {
-            bool hasAny = await  _context.Seller.AnyAsync(x => x.Id == obj.Id);
+            bool hasAny = await _context.Seller.AnyAsync(x => x.Id == obj.Id);
             if (!hasAny)
             {
                 throw new NotFoundException("Id not found");
